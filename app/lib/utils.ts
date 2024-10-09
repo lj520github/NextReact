c3bdf6f74ef899a731df6535eb04bc959c9c46c3
lj520github <JimmyYLJ@outlook.com>
2024-10-09 18:29:47 +0800

    币种图标修改

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,15 +1,15 @@
 import { Revenue } from './definitions';
 
 export const formatCurrency = (amount: number) => {
-  return (amount / 100).toLocaleString('en-US', {
+  return (amount / 100).toLocaleString('zh-CN', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'CNY',
   });
 };
 
 export const formatDateToLocal = (
   dateStr: string,
-  locale: string = 'en-US',
+  locale: string = 'zh-CN',
 ) => {
   const date = new Date(dateStr);
   const options: Intl.DateTimeFormatOptions = {
